91708a96f97dfde6b962c0fb6afcda9db1eb5971
takuyakawase <tkawase@netprotections.co.jp>
2016-06-16 01:29:41 +0000

    oo

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :messages, except: [:index, :new]# , only:[:create]
   #get 'messages/index'
 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
